91050de5109b01e26e706646175282c603f87ec2
Andrew Taylor <mac@andrewt.net>
2016-10-20 23:21:40 +0100

    Make it usable in the browser, because that's very much the point.
    Like, I was webpacking it before, I’m not a total berk.

--- framerate.js
+++ framerate.js
@@ -1,21 +1,23 @@
-module.exports = {
+// To run physics with a variable framerate, but
+// without ever running more than 100ms per frame:
+//
+// everyFrame(i => {
+//     /* simulate i milliseconds of physics */
+// }, 100);
+function everyFrame(callback, maxInterval, logFramerate) {
+	var lastTime = 0,
+		update = function everyFrameUpdate(time) {
+			var interval = time - lastTime;
+			if (logFramerate)
+				console.log((1000 / interval) + ' FPS');
+			lastTime = time;
+			requestAnimationFrame(update);
+			callback(Math.min(interval, maxInterval));
+		};
+	requestAnimationFrame(update);
+}
 
-	// To run physics with a variable framerate, but
-	// without ever running more than 100ms per frame:
-	// 
-	// everyFrame(i => {
-	//     /* simulate i milliseconds of physics */
-	// }, 100);
-	everyFrame: (callback, maxInterval) => {
-		let lastTime = 0,
-			update = time => {
-				let interval = time - lastTime;
-				// console.log((1000/interval) + ' FPS');
-				lastTime = time;
-				requestAnimationFrame(update);
-				callback(Math.min(interval, maxInterval));
-			};
-		requestAnimationFrame(update);
-	}
-
-};
+if (typeof module !== 'undefined')
+	module.exports = {
+		everyFrame: everyFrame
+	};
